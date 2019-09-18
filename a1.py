e734827e6e4c5432128962c13ea9df0b86f2c64c
noahjacknichols <noahjacknichols@gmail.com>
2019-09-17 21:02:29 -0400

    added convolution function

--- a1.py
+++ a1.py
@@ -10,3 +10,23 @@ print(imgArray.shape)
 # print(width)
 # print(height)
 
+
+def convolution(kernel, temp, arr):
+
+    width, height = imgArray.shape
+    temp = np.array([[0 for x in range(height + 2)] for y in range(width + 2)]).astype('int8')
+
+
+    return outArr
+
+
+
+
+
+
+kernel = [[-1,-1,-1],[-1,5,-1], [-1,-1,-1]] #KERNEL, IM DUMMY THICC
+
+
+
+outArr = convolution(kernel, imgArray)
+
