16778dbd7189cb4100ccea1ee9b8d1b40be137de
noahjacknichols <noahjacknichols@gmail.com>
2019-09-18 23:33:50 -0400

    created template functions needed for convolution

--- a1.py
+++ a1.py
@@ -7,25 +7,37 @@ img = Image.open("pepe.png")
 imgArray = np.array(img)
 print(imgArray.shape)
 
-# print(width)
-# print(height)
+def addBorder(imgArr):
+    temp = np.array([[0 for x in range(height + 2)] for y in range(width + 2)]).astype('int8')
+    width, height = imgArr.shape
+    for i in range(0, width):
+        for j in range(0,height):
+            temp = imgArr[i][j]
+            tmpArr[i+1][j+1] = temp
 
-
-def convolution(kernel, temp, arr):
+def convolution(kernel, imgArr):
 
     width, height = imgArray.shape
     temp = np.array([[0 for x in range(height + 2)] for y in range(width + 2)]).astype('int8')
+    outArr = imgArr
+    for i in range(0, width):
+        for j in range(0, height):
+            #convolution math
+            print()
 
 
     return outArr
 
 
 
-
+width, height = imgArray.shape
 
 
 kernel = [[-1,-1,-1],[-1,5,-1], [-1,-1,-1]] #KERNEL, IM DUMMY THICC
 
+
+
+temp = addBorder(imgArr)
 
 
 outArr = convolution(kernel, imgArray)
